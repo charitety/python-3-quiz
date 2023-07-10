3498b6f409d0972879e816836b27d2ff0c969cb3
Caridad RoblesCarvajal <croblescarvajal@xpanxion.com>
2023-07-10 11:01:48 -0400

    first commit

--- name_checker.py
+++ name_checker.py
@@ -1,13 +1,27 @@
 from ValidationException import ValidationException
+import re
+import ValidationException
+
 
 def validate_file(file_path):
-    pass #TODO: Only add code inside this function.
+    # read file
+    with open(file_path, "r") as inputFile:
+        # get first name of each line
+        for line in inputFile:
+            line = line.strip()
+            #using regular expression to get first name
+            firstName = re.split(",", line)[0]
+            print(firstName)
 
+            for i in firstName:
+                if i.isdigit():
+                    print(f'Invalid first name: {firstName}')
 
 def test():
     try:
         validate_file("users.txt")
     except ValidationException as ve:
         print(ve)
+
 
 test()
